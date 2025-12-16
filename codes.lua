d455323329abf6177954f4d501a1584080e2de70
ScripterNumber <infinixzedxxx@gmail.com>
2025-12-17 02:41:08 +0300

    Обновление codes.lua - 17.12.2025, 02:41:07

--- codes.lua
+++ codes.lua
@@ -3,15 +3,13 @@ return {
 		local playerData = Player:FindFirstChild("PlayerData")
 		if playerData then
 			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 3544487027502984)
-		end
-	end,
+		end,
 
 	['89161721131'] = function(Player:Player)
 		local playerData = Player:FindFirstChild("PlayerData")
 		if playerData then
 			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 1200059673376625)
-		end
-	end,
+		end,
 
 	['NEWSPVK2YEAR'] = function(Player:Player)
 		local playerData = Player:FindFirstChild("PlayerData")
@@ -19,7 +17,16 @@ return {
 			local coins = playerData:FindFirstChild("Coins")
 			if coins then
 				coins.Value = coins.Value + 3000
-			end
+			end,
+
+	['TEST123'] = function(Player:Player)
+	local playerData = Player:FindFirstChild("PlayerData")
+	if playerData then
+		local coins = playerData:FindFirstChild("Coins")
+		if coins then
+			coins.Value = coins.Value + 123
 		end
-	end,
+	end
+end
+
 }
