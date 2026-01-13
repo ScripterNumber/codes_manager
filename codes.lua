3a3a44b3754c528cf210a8ef0533ca26b4875cc1
ScripterNumber <infinixzedxxx@gmail.com>
2026-01-13 17:39:04 +0300

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -2,13 +2,13 @@ return {
 	['0MBR4D4BE5T'] = function(Player:Player)
 		local playerData = Player:FindFirstChild("PlayerData")
 		if playerData then
-			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 3544487027502984)
+			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 2989965499761875)
 		end,
 
 	['89161721131'] = function(Player:Player)
 		local playerData = Player:FindFirstChild("PlayerData")
 		if playerData then
-			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 1200059673376625)
+			require(game.ReplicatedStorage.Modules.BadgeAwarder).AwardPlayer(Player, 1770068872496833)
 		end,
 
 	['NEWSPVK2YEAR'] = function(Player:Player)
